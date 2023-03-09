9440565c9030449d6defbdfc54fe5bdb3f192ae4
Mohammad Ali <aliawan87@gmail.com>
2023-03-09 12:24:15 +0500

    Tic Tac Toe

---
+++ Assessment Prep/Written Assessment/lesson_4_questions/hard_1.rb
@@ -0,0 +1,66 @@
+module Trackable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+  
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Trackable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+class SeaCraft
+  include Trackable
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+    @propeller_count = propeller_count
+    @hull_count = hull_count
+  end
+
+  def range
+    super + 10
+  end
+end
+
+
+class Catamaran < Boat
+end
+
+class MotorBoat < Boat
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    super(1,1,km_traveled_per_liter, liters_of_fuel_capaci)
+  end
+end
--- Assessment Prep/test.rb
+++ Assessment Prep/test.rb
@@ -1,51 +1,19 @@
-class KrispyKreme
-  def initialize(filling_type, glazing)
-    self.filling_type = filling_type
-    self.glazing = glazing
-  end
 
-  def filling_type=(filling_type)
-    unless filling_type
-      @filling_type = "Plain"
-    else
-      @filling_type = filling_type
-    end
-  end
+WHEELS = "Vehicle"
+class Vehicle
 
-  def glazing=(glazing)
-    unless glazing
-      @glazing = ''
-    else
-      @glazing = glazing
-    end
-  end
-
-  def to_s
-    if @glazing.empty?
-      @filling_type
-    else
-      "#{@filling_type} with #{@glazing}"
-    end
-  end
 end
 
-donut1 = KrispyKreme.new(nil, nil)
-donut2 = KrispyKreme.new("Vanilla", nil)
-donut3 = KrispyKreme.new(nil, "sugar")
-donut4 = KrispyKreme.new(nil, "chocolate sprinkles")
-donut5 = KrispyKreme.new("Custard", "icing")
 
-puts donut1
-  # => "Plain"
 
-puts donut2
-  # => "Vanilla"
+module Inflatable
+	def inflate
+		puts "Inflated #{self.class::WHEELS} tires."
+	end
+end
 
-puts donut3
-  # => "Plain with sugar"
+class Car < Vehicle
+	include Inflatable
+end
 
-puts donut4
-  # => "Plain with chocolate sprinkles"
-
-puts donut5
-  # => "Custard with icing"
+Car.new.inflate
